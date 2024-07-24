577a4588fad39fa7103ff5db14add09a062eb704
govgovnahr <govindn3@illinois.edu>
2024-07-24 15:10:30 -0500

    template editing complete

---
+++ client/src/data/seo.js
@@ -0,0 +1,31 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"Hey, I'm Govind! I'm a software engineer with experience in AWS Cloud, React.js, and Python. I have a passion for building ",
+		keywords: ["Govind", "Govind N", "Govind Nair"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a software developer with expertise in React.js, Python, and AWS Cloud. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Govind", "Govind N", "Govind Nair"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Govind", "Govind N", "Govind Nair"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Govind", "Govind N", "Govind Nair"],
+	},
+];
+
+export default SEO;
---
+++ client/src/data/user.js
@@ -0,0 +1,68 @@
+
+
+const INFO = {
+	main: {
+		title: "Govind Nair",
+		name: "Govind N.",
+		email: "govindnair28@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/govgovnahr",
+		linkedin: "https://www.linkedin.com/in/govindnair2/",
+	},
+
+	homepage: {
+		title: "Govind Nair | AWS, React.js, Python",
+		description:
+			"Welcome to my personal website! I'm a software engineer with a passion for writing efficient, scalable, and reliable code. I have professional and project experience across a variety of technologies and I'm always looking for a new challenge!",
+	},
+
+	about: {
+		title: "I’m Govind. Nice to meet you!",
+		description:
+			" I'm a software engineer with a passion for writing efficient, scalable, and reliable code, and have professional and project experience across a variety of technologies. After graduating from the University of Illinois at Urbana-Champaign in May 2023 with a degree in Computational Physics and a minor in Computer Science, I've worked in the industry for nearly a year at Sony Interactive Entertainment. I'm always looking for a new challenge, and you can explore some of my work here!",
+	},
+
+	projects: [
+		{
+			title: "Time Machine",
+			description:
+				"A simple chatbot made to test the waters with LLMs. Built with Ollama, Python, and React.",
+			logo: "chatgpt.png",
+			linkText: "View Project",
+			link: "https://github.com/govgovnahr/TimeMachine",
+		},
+
+		{
+			title: "Where2Be",
+			description:
+				"An events app aimed at college students to find events on campus. I built the frontend with React Native.",
+			logo: "w2b.png",
+			linkText: "View Project",
+			link: "https://apps.apple.com/us/app/where2be-your-school-guide/id1671447709",
+		},
+
+		{
+			title: "Sony Interactive Entertainment",
+			description:
+				"Worked at Sony Interactive Entertainment for 1 year, working with AWS Cloud, LLMs, and Python.",
+			// logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			logo: "playstation.png",
+			// linkText: "View Project",
+			// link: "https://github.com",
+		},
+
+		// {
+		// 	title: "Project 5",
+		// 	description:
+		// 		"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+		// 	linkText: "View Project",
+		// 	link: "https://github.com",
+		// },
+	],
+};
+
+export default INFO;
